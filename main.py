4bcd05e7d250609583e1f03ecc8b207f35d95d97
Zacffor <hartzelec@gmail.com>
2026-05-26 10:52:02 +0000

    fix main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
