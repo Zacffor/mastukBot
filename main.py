adfa2244ed136dfd69d25fc1a7d96f45b623d763
Zacffor <hartzelec@gmail.com>
2026-05-31 09:16:11 +0000

    Fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Halo! Bot aktif 👋")
   
 async def balas(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  teksupdate.message.reply_text(f"Kamu bilang: {teks}")
+  await update.message.reply_text(f"Kamu bilang: {teks}")
 
 app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 app.add_handler(CommandHandler("start", start))
